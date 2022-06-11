6359e58ffc7a943597e326d2920d7a614dff4f68
Jeff Campbell <inajiff@gmail.com>
2022-06-11 07:53:46 -0700

    updating url of team stats

--- fantasy/calculator/settings/api.py
+++ fantasy/calculator/settings/api.py
@@ -3,32 +3,32 @@ from datetime import datetime
 
 year = datetime.now().strftime('%Y')
 today = datetime.now().strftime('%Y/%m/%d')
-year = '2019'
+year = '2022'
 # today = '2019/10/30'
 
 YEAR_FOR_HITTING_STATS = os.getenv('YEAR_FOR_HITTING_STATS', year)
 DATE_FOR_STANDINGS = os.getenv('DATE_FOR_STANDINGS', today)
 
 BASE_URL = 'http://mlb.mlb.com/lookup/json/'
-UPDATED_BASE_URL = 'http://lookup-service-prod.mlb.com/json/'
+UPDATED_BASE_URL = 'https://bdfed.stitch.mlbinfra.com/bdfed/stats/team'
 
-TEAM_HITTING_JSON_BLOCK = 'team_hitting_season_leader_master'
+TEAM_HITTING_JSON_BLOCK = '?stitch_env=prod&sportId=1&gameType=R&group=hitting&order=desc&sortStat=onBasePlusSlugging&stats=season'
 
-TEAM_STATS_URI = TEAM_HITTING_JSON_BLOCK + '?season=' \
+TEAM_STATS_URI = TEAM_HITTING_JSON_BLOCK + '&season=' \
     + YEAR_FOR_HITTING_STATS \
-    + '&sort_order=%27desc%27&sort_column=%27avg%27&game_type=%27R%27&sport_code=%27mlb%27'
+    + '&limit=30&offset=0'
 
 # SPLITS_URI = 'named.team_hitting_season_leader_sit.bam?season=' \
 SPLITS_URI =  TEAM_HITTING_JSON_BLOCK + '?season=' \
     + YEAR_FOR_HITTING_STATS \
     + '&sort_order=%27desc%27&sort_column=%27avg%27&game_type=%27R%27&sport_code=%27mlb%27'
+# This may no longer be necessary but doesn't seem to stop data.
 END_QUAL = '&recSP=1&recPP=50'
-# Home Away splits different?
-# http://lookup-service-prod.mlb.com/json/named.team_hitting_season_leader_sit.bam?season=2019&sort_order=%27desc%27&sort_column=%27avg%27&game_type=%27R%27&sport_code=%27mlb%27&sit_code=%27vr%27&recSP=1&recPP=50
-TEAM_AT_HOME_URI   = SPLITS_URI + '&sit_code=%27h%27' + END_QUAL
-TEAM_AWAY_URI      = SPLITS_URI + '&sit_code=%27a%27' + END_QUAL
-TEAM_VS_LEFTY_URI  = SPLITS_URI + '&sit_code=%27vl%27' + END_QUAL
-TEAM_VS_RIGHTY_URI = SPLITS_URI + '&sit_code=%27vr%27' + END_QUAL
+
+TEAM_AT_HOME_URI   = SPLITS_URI + '&sitCodes=h' + END_QUAL
+TEAM_AWAY_URI      = SPLITS_URI + '&sitCodes=a' + END_QUAL
+TEAM_VS_LEFTY_URI  = SPLITS_URI + '&sitCodes=vl' + END_QUAL
+TEAM_VS_RIGHTY_URI = SPLITS_URI + '&sitCodes=v4' + END_QUAL
 
 TEAM_STANDING_URI = \
     'named.standings_schedule_date.bam?season=' \
